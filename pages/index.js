a0369470adc54dab0ce1bb31e6e311e9deb1e8d9
Alessandro Lima <alessandrolima93@gmail.com>
2021-02-04 03:35:11 -0300

    wip

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,20 @@ export default function Home({user}) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Teste</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Portfolio Lima</title>
+        <link href="//db.onlinewebfonts.com/c/2545d122b16126676225a5b52283ae23?family=VCR+OSD+Mono" rel="stylesheet" type="text/css"/>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to portfolio do Noilerua &#9733;
-        </h1>
+        <img className={styles.avatar} src={user[0].owner.avatar_url} width="150" height="150" alt="limaale"/>
+        <span className={styles.title}>{user[0].owner.login}</span>
+        <span>Projetos</span>
+        <span style={{fontFamily: 'DM Mono', color:'#56f5ff'}}>&darr;</span>
         {user.map((item)=>(
-          <span>{item.name}</span>
+          <span className={styles.projectsTitle}>{item.name}</span>
         ))}
-       
+        <span>under development ⚠️</span>
+        <img src="/ff12_cursor.png" alt="cursor"/>
       </main>
     </div>
   )
